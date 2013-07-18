94b603fb510d9f2fd0a7e8a404c072dca4808844
Marcelo Manzan <manzan@gmail.com>
2013-07-18 12:49:41 -0300

    customized settings for projects

---
+++ lib/default_settings.sh
@@ -0,0 +1,10 @@
+STEPUP_ARCHIVING_SECTION="versioning"
+STEPUP_ARCHIVING_MESSAGE="available on {release}"
+
+STEPUP_RELEASE_MASK="v{major}.{minor}.{patch}.{tiny?}"
+# STEPUP_PRERELEASE_SEQUENCE=".alpha{alpha} .beta{beta} .rc{rc}"
+
+STEPUP_NOTES_SECTIONS="features>minor bugfixes>patch changes>patch"
+STEPUP_NOTES_SECTION_LABEL_features="Features:"
+STEPUP_NOTES_SECTION_LABEL_bugfixes="Bugfixes:"
+STEPUP_NOTES_SECTION_LABEL_changes="Changes:"
