18e372c952f8c2bb1f0826c59d3c8509fa277280
Marcelo Manzan <manzan@gmail.com>
2013-08-02 18:38:10 -0300

    sub-command of "version-help"

---
+++ lib/version-help.sh
@@ -0,0 +1,5 @@
+case $1 in
+    *)
+        die "Invalid sub-command: $1"
+    ;;
+esac
--- lib/version.sh
+++ lib/version.sh
@@ -1,1 +1,12 @@
 source $STEPUP_ROOT/_git_check.sh
+
+SUB_COMMAND=$1
+case $1 in
+    help)
+        shift
+        source $STEPUP_ROOT/version-$SUB_COMMAND.sh
+    ;;
+    *)
+        # source $STEPUP_ROOT/version-show.sh
+    ;;
+esac
